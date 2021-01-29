799b22b870114c1871159bc52a2726f816343552
Юлия Чечульникова <y.chechulnikova@tinkoff.ru>
2021-01-29 21:36:39 +0300

    cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
@@ -15,33 +15,33 @@ const { DB } = require('./config');
 
 const { PORT = 3000 } = process.env;
 
-// const whitelist = [
-//   'http://localhost:8080',
-//   'https://yuliya-pavlova.github.io',
-//   'https://www.mycoolnews.students.nomoreparties.space',
-//   'http://www.mycoolnews.students.nomoreparties.space',
-//   'https://mycoolnews.students.nomoreparties.space',
-// ];
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: [
-//     'Content-Type',
-//     'origin',
-//     'x-access-token',
-//     'authorization',
-//     'credentials',
-//   ],
-//   credentials: true,
-// };
+const whitelist = [
+  'http://localhost:8080',
+  'https://yuliya-pavlova.github.io',
+  'https://www.mycoolnews.students.nomoreparties.space',
+  'http://www.mycoolnews.students.nomoreparties.space',
+  'https://mycoolnews.students.nomoreparties.space',
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: [
+    'Content-Type',
+    'origin',
+    'x-access-token',
+    'authorization',
+    'credentials',
+  ],
+  credentials: true,
+};
 
 const app = express();
 
@@ -55,7 +55,7 @@ mongoose.connect(DB, {
 //   windowMs: 15 * 60 * 1000,
 //   max: 200,
 // });
-// app.use('*', cors(corsOptions));
+app.use('*', cors(corsOptions));
 app.use(bodyParser());
 app.use(helmet());
 app.use(cookieParser());

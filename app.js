ea5bd2b28ad143c5f8d88915ac0b53d1ac9432ae
Юлия Чечульникова <y.chechulnikova@tinkoff.ru>
2021-01-29 16:48:26 +0300

    cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
@@ -15,28 +15,28 @@ const { DB } = require('./config');
 
 const { PORT = 3000 } = process.env;
 
-const corsOptions = {
-  origin: [
-    'http://localhost:8080',
-    'https://localhost:8080',
-    'http://mycoolnews.students.nomoreparties.space',
-    'https://mycoolnews.students.nomoreparties.space',
-    'http://www.mycoolnews.students.nomoreparties.space',
-    'https://www.mycoolnews.students.nomoreparties.space',
-    'https://yuliya-pavlova.github.io/news-explorer-frontend',
-  ],
-  methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'HEAD'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: [
-    'Content-Type',
-    'origin',
-    'x-access-token',
-    'authorization',
-    'credentials',
-  ],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:8080',
+//     'https://localhost:8080',
+//     'http://mycoolnews.students.nomoreparties.space',
+//     'https://mycoolnews.students.nomoreparties.space',
+//     'http://www.mycoolnews.students.nomoreparties.space',
+//     'https://www.mycoolnews.students.nomoreparties.space',
+//     'https://yuliya-pavlova.github.io/news-explorer-frontend',
+//   ],
+//   methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'HEAD'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: [
+//     'Content-Type',
+//     'origin',
+//     'x-access-token',
+//     'authorization',
+//     'credentials',
+//   ],
+//   credentials: true,
+// };
 
 const app = express();
 
@@ -53,17 +53,21 @@ const limiter = rateLimit({
 
 // app.use(cors);
 app.use(bodyParser());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTION') {
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, HEAD');
+    return res.status(200).json({});
+  }
+});
 app.use(helmet());
 app.use(cookieParser());
 app.use(requestLogger);
 
 app.use(limiter);
-app.use('*', cors(corsOptions));
+// app.use('*', cors(corsOptions));
 app.use(routes);
-
-app.use((req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-});
 
 app.use(errorLogger);
 app.use(errors());

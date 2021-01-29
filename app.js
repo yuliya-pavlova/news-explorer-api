b766d66fde11b647450853d2d6095bffa1664fa8
Юлия Чечульникова <y.chechulnikova@tinkoff.ru>
2021-01-29 17:23:06 +0300

    cirs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
@@ -15,42 +15,44 @@ const { DB } = require('./config');
 
 const { PORT = 3000 } = process.env;
 
-// const corsOptions = {
-//   origin: [
-//     'http://localhost:8080',
-//     'https://localhost:8080',
-//     'http://mycoolnews.students.nomoreparties.space',
-//     'https://mycoolnews.students.nomoreparties.space',
-//     'http://www.mycoolnews.students.nomoreparties.space',
-//     'https://www.mycoolnews.students.nomoreparties.space',
-//     'https://yuliya-pavlova.github.io/news-explorer-frontend',
-//   ],
-//   methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'HEAD'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: [
-//     'Content-Type',
-//     'origin',
-//     'x-access-token',
-//     'authorization',
-//     'credentials',
-//   ],
-//   credentials: true,
-// };
+const whitelist = [
+  'http://localhost:8080',
+  'https://yuliya-pavlova.github.io',
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: [
+    'Content-Type',
+    'origin',
+    'x-access-token',
+    'authorization',
+    'credentials',
+  ],
+  credentials: true,
+};
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://yuliya-pavlova.github.io');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://yuliya-pavlova.github.io');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
 
-  app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-    res.send();
-  });
-});
+//   app.options('*', (req, res) => {
+//     res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+//     res.send();
+//   });
+// });
 
 mongoose.connect(DB, {
   useNewUrlParser: true,
@@ -62,24 +64,13 @@ mongoose.connect(DB, {
 //   windowMs: 15 * 60 * 1000,
 //   max: 200,
 // });
-
-// app.use(cors);
+app.use('*', cors(corsOptions));
 app.use(bodyParser());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   if (req.method === 'OPTION') {
-//     res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, HEAD');
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
 app.use(helmet());
 app.use(cookieParser());
 app.use(requestLogger);
 
 // app.use(limiter);
-// app.use('*', cors(corsOptions));
 app.use(routes);
 
 app.use(errorLogger);

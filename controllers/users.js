d1f34e78defa10476642bb7c88f04c32a13de320
Юлия Чечульникова <y.chechulnikova@tinkoff.ru>
2021-01-29 18:18:39 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -56,7 +56,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '8d' });
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
